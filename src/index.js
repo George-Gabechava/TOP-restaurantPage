70acc10c9d4ebd9eabfa6fdb5ec99e5c7c9b1d91
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2023-10-12 11:14:29 -0400

    up to date files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './style.css';
 
+
+////Make a new branch to mess with these imports
+////try to just do import './tab1.js' with no default functions?
+
 ////Unable to get index.js to import files
 
 // import tab1Function from './tab1.js';
