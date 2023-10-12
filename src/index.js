148938e09cd0fc4f4a2cb8850a146887859c591a
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2023-10-12 14:52:24 -0400

    index.js might not have been properly changed in vscode

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import './style.css';
-
-
-////Make a new branch to mess with these imports
-////try to just do import './tab1.js' with no default functions?
-
-////Unable to get index.js to import files
-
-// import tab1Function from './tab1.js';
-// import tab2Function from './tab2.js';
-
-// const button1 = document.getElementById("tab1");
-// button1.addEventListener('click', tab1Function)
-
-// const button2 = document.getElementById("tab2");
-// button2.addEventListener('click', tab2Function);
-
-=======
->>>>>>> develop
 console.log('Hello Worlds!');
 
 import './style.css';
